bffa330c70867cbc37dc8a58f052e4b6bb5b859c
penn <penn6699@126.com>
2018-06-02 18:32:50 +0800

    3

--- Web/App_Code/Handler.cs
+++ Web/App_Code/Handler.cs
@@ -6,15 +6,11 @@ using System.Text;
 
 public class Handler : IHttpHandler
 {
-
+    public bool IsReusable { get { return false; } }
     public void ProcessRequest(HttpContext context)
     {
         context.Response.ContentType = "text/plain";
         context.Response.Write("Hello World");
-        
-        Stream ms = MyWebRequest.Get("https://www.cnblogs.com/xingzc/p/5986464.html");
-        StreamReader sr = new StreamReader(ms, Encoding.UTF8);
-        string re = sr.ReadToEnd();
 
         string path = context.Server.MapPath("~/Data");
         if (!Directory.Exists(path))
@@ -22,40 +18,49 @@ public class Handler : IHttpHandler
             Directory.CreateDirectory(path);
         }
 
-        //FileStream fs = File.Open(path + "/5986464.html", FileMode.Append);
-        StreamWriter sw = new StreamWriter(File.Open(path + "/" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".html", FileMode.Append));
-        sw.Write(re);
-        sw.Flush();
-        sw.Close();
-        sw.Dispose();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-    public bool IsReusable
-    {
-        get
+        using (Stream ms = MyWebRequest.Get("https://www.cnblogs.com/xingzc/p/5986464.html"))
         {
-            return false;
+            using (StreamReader sr = new StreamReader(ms, Encoding.UTF8))
+            {
+                string re = sr.ReadToEnd();
+                StreamWriter sw = new StreamWriter(File.Open(path + "/https-" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".html", FileMode.Append));
+                sw.Write(re);
+                sw.Flush();
+                sw.Close();
+                sw.Dispose();
+            }
+            ms.Close();
         }
+
+        using (Stream ms = MyWebRequest.Get("http://www.jyubbs.com/bbs/forum.php"))
+        {
+            using (StreamReader sr = new StreamReader(ms))
+            {
+                string re = sr.ReadToEnd();
+                StreamWriter sw = new StreamWriter(File.Open(path + "/http-" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".html", FileMode.Append));
+                sw.Write(re);
+                sw.Flush();
+                sw.Close();
+                sw.Dispose();
+            }
+            ms.Close();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 
 }
